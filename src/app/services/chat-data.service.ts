ee57272d19ab5d0ce74dbbd35eba0770efb13d11
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-03-31 15:12:48 -0500

    fix: use Glutent-API

--- src/app/services/chat-data.service.ts
+++ src/app/services/chat-data.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '../models/message.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatDataService {
-
+  private url : string = environment.url;
   constructor(private httpClient: HttpClient) { }
 
   sendMessage(data : any){
-    return this.httpClient.post<Message>("http://127.0.0.1:5000",data);
+    return this.httpClient.post<Message>(this.url,data);
   }
 }
